e519c6d2b0a80e65ff159ae51ae2e489ea0df738
Joao Vitor Aguiar <81645804+joao-v170r@users.noreply.github.com>
2022-09-23 22:11:44 -0300

    Add files via upload

---
+++ CAluno.aspx
@@ -0,0 +1,65 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/PrincipalMaster.Master" AutoEventWireup="true" CodeBehind="CAluno.aspx.cs" Inherits="app_escolar.CAluno" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="conteinerCentral">
+        <asp:UpdatePanel id="updatePainel1" runat="server">
+            <ContentTemplate>
+                <div class="card efeito_Panel_2">
+                  <div class="card-body bg-dark text-white">
+                    <h5 class="card-title">Cadastro Alunos</h5>
+                      <div class="card-body">
+                          <div class="form-group">
+                              <asp:Label ID="Label1" runat="server" Text="Nome"></asp:Label>
+                              <asp:TextBox CssClass="form-control" ID="txtNome" runat="server"></asp:TextBox>
+                          </div>
+                          <div class="row">
+                              <div class="col-3">
+                                <asp:Label ID="Label2" runat="server" Text="Telefone"></asp:Label>
+                                <asp:TextBox CssClass="form-control" ID="TextFone" runat="server"></asp:TextBox>
+                              </div>
+                              <div class="col-3">
+                                  <asp:Label ID="Label3" runat="server" Text="CPF"></asp:Label>
+                                <asp:TextBox CssClass="form-control" ID="txtCPF" runat="server"></asp:TextBox>
+                              </div>
+                              <div class="col-3">
+                                  <asp:Label ID="Label4" runat="server" Text="Data Nascimento"></asp:Label>
+                                <asp:TextBox CssClass="form-control" ID="TextDate" type="date" runat="server"></asp:TextBox>
+                              </div>
+                              <div class="col-3">
+                                  <asp:Label ID="Label5" runat="server" Text="e-mail"></asp:Label>
+                                <asp:TextBox CssClass="form-control" ID="TextEmail" type="email" runat="server"></asp:TextBox>
+                              </div>
+                          </div>
+                          <div class="form-group">
+                              <asp:Label ID="Label6" runat="server" Text="Nome da Mãe"></asp:Label>
+                              <asp:TextBox CssClass="form-control" ID="TextNomeMae" runat="server"></asp:TextBox>
+                          </div>
+                          <div class="form-group">
+                              <asp:Label ID="Label7" runat="server" Text="Nome do Pai"></asp:Label>
+                              <asp:TextBox CssClass="form-control" ID="TextNomePai" runat="server"></asp:TextBox>
+                          </div>
+                      </div>
+                      <div class="card-footer">
+                        <div class="form-group">
+                            <div class="form-group row">
+                                <div class="col-md-3">
+                                    <asp:Button ID="cmdConfirmar" runat="server" CssClass="btn btn-success" Text="Incluir" Font-Bold="true" Width="100%"  />
+                                </div>
+                                <div class="col-md-3">
+                                    <asp:Button ID="cmdExluir" runat="server" CssClass="btn btn-success" Text="Excluir" Font-Bold="true" Width="100%" Enabled="False" />
+                                </div>
+                                <div class="col-md-3">
+                                    <asp:Button ID="cmdLimpar" runat="server" CssClass="btn btn-success" Text="Limpar" Font-Bold="true" Width="100%"/>
+                                </div>
+                                <div class="col-md-3">
+                                    <asp:Button ID="cmdSair" runat="server" CssClass="btn btn-success" Text="Sair" Font-Bold="true" Width="100%" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                  </div>
+                </div>
+            </ContentTemplate>
+        </asp:UpdatePanel>
+    </div>
+
+</asp:Content>
---
+++ CAluno.aspx.designer.cs
@@ -0,0 +1,188 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Esse código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace app_escolar
+{
+
+
+    public partial class CAluno
+    {
+
+        /// <summary>
+        /// Controle updatePainel1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel updatePainel1;
+
+        /// <summary>
+        /// Controle Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Controle txtNome.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNome;
+
+        /// <summary>
+        /// Controle Label2.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+
+        /// <summary>
+        /// Controle TextFone.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextFone;
+
+        /// <summary>
+        /// Controle Label3.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+
+        /// <summary>
+        /// Controle txtCPF.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCPF;
+
+        /// <summary>
+        /// Controle Label4.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
+
+        /// <summary>
+        /// Controle TextDate.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextDate;
+
+        /// <summary>
+        /// Controle Label5.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label5;
+
+        /// <summary>
+        /// Controle TextEmail.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextEmail;
+
+        /// <summary>
+        /// Controle Label6.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label6;
+
+        /// <summary>
+        /// Controle TextNomeMae.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextNomeMae;
+
+        /// <summary>
+        /// Controle Label7.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label7;
+
+        /// <summary>
+        /// Controle TextNomePai.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextNomePai;
+
+        /// <summary>
+        /// Controle cmdConfirmar.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button cmdConfirmar;
+
+        /// <summary>
+        /// Controle cmdExluir.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button cmdExluir;
+
+        /// <summary>
+        /// Controle cmdLimpar.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button cmdLimpar;
+
+        /// <summary>
+        /// Controle cmdSair.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button cmdSair;
+    }
+}
